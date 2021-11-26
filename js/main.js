1b44b7d364e8c9747e83cef899c466d72a5fd489
Muhammad Ahmed <moahzed@gmail.com>
2021-11-25 23:04:18 -0500

    Initial Code Commit
    Initialized all necessary files and folders for the project. Developed directory structure.
    
    Signed-off-by: Muhammad Ahmed <moahzed@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+    =================================================
+    Author: Muhammad Ahmed (4zad)
+    Copyright (c) 2021 Muhammad Ahmed (4zad).
+    All rights reserved.
+    =================================================
+*/
+
