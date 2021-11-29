89a4bb38600d2c95f864493d231507940f018cba
Muhammad Ahmed <moahzed@gmail.com>
2021-11-28 21:18:55 -0500

    Added Scroll Event to Show Current Section of Page on Navigation Bar
    Modified 'index.html', style.css, and wrote into 'main.js' to have a marker show up under each navigation link in the navbar when it's respective section is in the viewport. This feature is still unfinished and buggy. More code is required in 'main.js'. Lines of code utilised for testing have been left commented, incase of their future need.
    
    Signed-off-by: Muhammad Ahmed <moahzed@gmail.com>

--- js/main.js
+++ js/main.js
@@ -6,7 +6,39 @@
     =================================================
 */
 
-const toggleMenu = (status) =>
-{
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav_bar > nav_links > ul li a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    // console.log(scrollY); // 'scrollY' and 'pageYOffset' display the position of your scroll from the top of the webpage, or in otherwords how much you have scrolled
+    // 'pageYOffset' is an alias for 'scrollY'
+
+    sections.forEach((section) => {
+        const currentSectionTop = section.offsetTop;
+        // console.log(eachSectionTop); // test to see if 'section.offsetTop' is returning the correct values of how far the beginning/top of each section is from the beginning/top of the webpage
+        const currentSectionHeight = section.clientHeight;
+
+        if (scrollY >= currentSectionTop) {
+            current = section.getAttribute('id');
+        }
+    })
+    // console.log(current); // 'current' should be holding the id of the section element that is on the viewport; this is a test to see if that is working
+
+    navLinks.forEach((a) => {
+        a.classList.remove('active');
+
+        if (a.classList.contains(current)) {
+            a.classList.add('active');
+            console.log('changed');
+        }
+    })
+});
+
+
+
+// const toggleMenu = (status) =>
+// {
     
-}
+// }
+
