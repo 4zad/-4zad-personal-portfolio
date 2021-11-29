bf60f932cb552ced1a5be6278fc0b1263c201a92
Muhammad Ahmed <moahzed@gmail.com>
2021-11-28 22:51:20 -0500

    Completed Script for Scroll Event to Indicate Current Section of Page on Navigation Bar
    Modified the script in 'main.js' that creates a marker under each navigation link in the navbar when it's respective section is in the viewport. This feature is now complete and fully functional. Lines of code utilised for testing have been still been left commented, incase of their future need.
    
    Signed-off-by: Muhammad Ahmed <moahzed@gmail.com>

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@
 */
 
 const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.nav_bar > nav_links > ul li a');
+const navLinks = document.querySelectorAll('.nav_bar > .nav_links > ul li > a');
+// console.log(sections); // test to see if `document.querySelectorAll('section')` was successful
+// console.log(navLinks); // test to see if `document.querySelectorAll('.nav_bar > .nav_links > ul li a')` was successful
 
 window.addEventListener('scroll', () => {
     let current = '';
@@ -19,7 +21,7 @@ window.addEventListener('scroll', () => {
         // console.log(eachSectionTop); // test to see if 'section.offsetTop' is returning the correct values of how far the beginning/top of each section is from the beginning/top of the webpage
         const currentSectionHeight = section.clientHeight;
 
-        if (scrollY >= currentSectionTop) {
+        if (scrollY >= (currentSectionTop - (currentSectionHeight * 2 / 5))) {
             current = section.getAttribute('id');
         }
     })
@@ -30,7 +32,7 @@ window.addEventListener('scroll', () => {
 
         if (a.classList.contains(current)) {
             a.classList.add('active');
-            console.log('changed');
+            // console.log('active class placement changed'); // test to see if the program enters the if statement, signaling that the 'current' id fo the section shown in the viewport has a link with a matching class name
         }
     })
 });
